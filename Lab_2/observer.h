2bcbddc720bfd64bb1264fcf49ba25a1b58494d3
Kris <Valentina10@sibmail.com>
2022-05-12 10:20:27 +0700

    Creating an FSubject class

--- Lab_2/observer.h
+++ Lab_2/observer.h
@@ -1,7 +1,7 @@
 #ifndef OBSERVER_H
 #define OBSERVER_H
 #include <iostream>
-
+#include <QFile>
 #include <QMainWindow>
 
 class Observer
@@ -21,5 +21,21 @@ void FileObserver::Update()
    std::cout << "File was removed" << "\n";
 }
 
+class FSubject
+{
+    std::vector<FileObserver*> list;
+
+    public:
+    std::string filepath;
+    std::QFile file;
+    bool fileexist;
+    float filesize;
+
+    void attach(FileObserver *isfile);
+    void detach(FileObserver *isfile);
+    void notify();
+    bool fileExist();
+    bool sizeChanged();
+};
 
 #endif // OBSERVER_H
