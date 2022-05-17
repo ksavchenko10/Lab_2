5c544c48ab0796fa46f2e5f2b20cd21cd1bd0e56
Kris <Valentina10@sibmail.com>
2022-05-17 11:59:13 +0700

    Adding comments to the main.cpp file

--- Lab_2_1/main.cpp
+++ Lab_2_1/main.cpp
@@ -8,12 +8,12 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    FileObserver* obs;
+    FileObserver* obs; //создаем экземпляр наблюдателя
 
-    FSubject file("./test.txt");
+    FSubject file("./test.txt"); //создаем экзепляр состояния файла
 
-    file.attach(obs);
-    file.startNotify();
+    file.attach(obs); //добавляем наблюдателя к экземпляру состояния файла
+    file.startNotify(); //запускаем постоянную проверку состояния файла
 
     return a.exec();
 }
